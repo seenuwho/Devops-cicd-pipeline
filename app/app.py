5fe057e4d8684553d9701a2a3d74ab7f51ccf196
Sreenivasulu <seenu.who@gmail.com>
2026-08-23 23:37:36 +0530

    Restore Flask application

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello From Devops CI/CD Pipeline"
+    return "Hello From Automated Devops CI/CD Deployment"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
