4e3e6983492f57ba01f1d617b7e2ed308e56b77a
Sreenivasulu <seenu.who@gmail.com>
2026-08-23 22:31:56 +0530

    Convert app to Flask web application

--- app/app.py
+++ app/app.py
@@ -1,1 +1,10 @@
-print("Hello From Devops CI/CD Pipeline")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello From Devops CI/CD Pipeline"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
